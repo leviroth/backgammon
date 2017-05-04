4cd4b72861a2e51ec05df585a1575c11cca9c654
Levi Roth <levimroth@gmail.com>
2017-05-04 15:19:11 -0400

    Add basic board manipulation functions

--- src/board.ml
+++ src/board.ml
@@ -30,10 +30,36 @@ let get_point board n =
   |> Option.value_exn
 ;;
 
-List.map ~f:(fun n ->
-    n
-    |> (fun n -> get_point starting_board n)
-    |> sexp_of_option (sexp_of_pair sexp_of_color sexp_of_int)
-    |> Sexplib.Sexp.to_string
-    |> print_endline)
-  (List.range 1 25)
+let get_contents =
+  Location.Map.find_exn
+
+let remove_from board ?(n=1) location =
+  let update_fn = function
+    | Some (color, count) -> if count = n then None else Some (color, count - n)
+    | None -> None in
+  Location.Map.change board location ~f:(Option.map ~f:update_fn)
+;;
+
+let add_to board ?(n=1) location color =
+  let update_fn = function
+    | Some (old_color, count) when old_color = color -> Some(color, count + n)
+    | Some (_, _) | None -> Some(color, n) in
+  Location.Map.change board location ~f:(Option.map ~f:update_fn)
+;;
+
+let move board source dest =
+  Option.map (Location.Map.find_exn board source)
+    ~f:(fun (color, _) -> (board
+                          |> (fun x -> remove_from x source)
+                          |> (fun x -> add_to x dest color)))
+;;
+
+let () =
+  let b = Option.value_exn (move starting_board (Location.Point 1) (Location.Point 2)) in
+  List.iter ~f:(fun n ->
+      n
+      |> (fun n -> get_point b n)
+      |> sexp_of_option (sexp_of_pair sexp_of_color sexp_of_int)
+      |> Sexplib.Sexp.to_string
+      |> print_endline)
+    (List.range 1 25)
--- src/board.mli
+++ src/board.mli
@@ -1,3 +1,7 @@
+open Color
+
 type t;;
 
 val starting_board : t;;
+
+val get_contents : t -> Location.t -> (color * int) option
